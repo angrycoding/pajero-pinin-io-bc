bfd8865abac773e273afbb96cfd58e63e6523f1f
Ruslan <matveev.ruslan@gmail.com>
2017-04-11 16:31:53 +0300

    todo

--- pajero-pinin-io-lcd-slave.ino
+++ pajero-pinin-io-lcd-slave.ino
@@ -29,35 +29,54 @@
 // расход, л/100км
 #define CONSUMPTION_L100KM 0b01000100
 
-
-#define T_D1(value, bit) ((value >> bit & 1) << 0)
-#define C_D1(value, bit) ((value >> bit & 1) << 1)
-#define B_D1(value, bit) ((value >> bit & 1) << 2)
-#define TL_D1(value, bit) ((value >> bit & 1) << 3)
-#define TR_D1(value, bit) ((value >> bit & 1) << 4)
-#define BL_D1(value, bit) ((value >> bit & 1) << 5)
-#define BR_D1(value, bit) ((value >> bit & 1) << 6)
-
-#define T_D2(value, bit) ((value >> bit & 1) << 7)
-#define C_D2(value, bit) ((value >> bit & 1) << 8)
-#define B_D2(value, bit) ((value >> bit & 1) << 9)
-#define TL_D2(value, bit) ((value >> bit & 1) << 10)
-#define TR_D2(value, bit) ((value >> bit & 1) << 11)
-#define BL_D2(value, bit) ((value >> bit & 1) << 12)
-#define BR_D2(value, bit) ((value >> bit & 1) << 13)
-
-#define T_D3(value, bit) ((value >> bit & 1) << 14)
-#define C_D3(value, bit) ((value >> bit & 1) << 15)
-#define B_D3(value, bit) ((value >> bit & 1) << 16)
-#define TL_D3(value, bit) ((value >> bit & 1) << 17)
-#define TR_D3(value, bit) ((value >> bit & 1) << 18)
-#define BL_D3(value, bit) ((value >> bit & 1) << 19)
-#define BR_D3(value, bit) ((value >> bit & 1) << 20)
-
-#define D4(value, bit) ((value >> bit & 1) << 21)
-#define FLOAT_D(value, bit) ((value >> bit & 1) << 22)
-#define NEGATIVE_D(value, bit) ((value >> bit & 1) << 23)
-
+// единицы - верхний сегмент
+#define D0_T(value, bit) ((value >> bit & 1) << 0)
+// единицы - средний сегмент
+#define D0_C(value, bit) ((value >> bit & 1) << 1)
+// единицы - нижний сегмент
+#define D0_B(value, bit) ((value >> bit & 1) << 2)
+// единицы - верхний левый сегмент
+#define D0_TL(value, bit) ((value >> bit & 1) << 3)
+// единицы - верхний правый сегмент
+#define D0_TR(value, bit) ((value >> bit & 1) << 4)
+// единицы - нижний левый сегмент
+#define D0_BL(value, bit) ((value >> bit & 1) << 5)
+// единицы - нижний правый сегмент
+#define D0_BR(value, bit) ((value >> bit & 1) << 6)
+// десятки - верхний сегмент
+#define D1_T(value, bit) ((value >> bit & 1) << 7)
+// десятки - средний сегмент
+#define D1_C(value, bit) ((value >> bit & 1) << 8)
+// десятки - нижний сегмент
+#define D1_B(value, bit) ((value >> bit & 1) << 9)
+// десятки - верхний левый сегмент
+#define D1_TL(value, bit) ((value >> bit & 1) << 10)
+// десятки - верхний правый сегмент
+#define D1_TR(value, bit) ((value >> bit & 1) << 11)
+// десятки - нижний левый сегмент
+#define D1_BL(value, bit) ((value >> bit & 1) << 12)
+// десятки - нижний правый сегмент
+#define D1_BR(value, bit) ((value >> bit & 1) << 13)
+// сотни - верхний сегмент
+#define D2_T(value, bit) ((value >> bit & 1) << 14)
+// сотни - средний сегмент
+#define D2_C(value, bit) ((value >> bit & 1) << 15)
+// сотни - нижний сегмент
+#define D2_B(value, bit) ((value >> bit & 1) << 16)
+// сотни - верхний левый сегмент
+#define D2_TL(value, bit) ((value >> bit & 1) << 17)
+// сотни - верхний правый сегмент
+#define D2_TR(value, bit) ((value >> bit & 1) << 18)
+// сотни - нижний левый сегмент
+#define D2_BL(value, bit) ((value >> bit & 1) << 19)
+// сотни - нижний правый сегмент
+#define D2_BR(value, bit) ((value >> bit & 1) << 20)
+// тысячи - контроллируется одним сегментом
+#define D3(value, bit) ((value >> bit & 1) << 21)
+// десятичный разделитель между десятками и единицами
+#define D_FLOAT(value, bit) ((value >> bit & 1) << 22)
+// знак минуса
+#define D_NEGATIVE(value, bit) ((value >> bit & 1) << 23)
 
 
 uint32_t RAW_TIME;
@@ -76,7 +95,7 @@ volatile uint8_t SPI_METERAGE_UNIT;
 void setup() {
 	SPI_STATE = SPI_STATE_START;
 	Serial.begin(250000);
-	SPCR |= bit (SPE);
+	SPCR |= bit(SPE);
 	SPI.setBitOrder(LSBFIRST);
 	SPI.setDataMode(SPI_MODE3);
 	SPI.attachInterrupt();
@@ -97,6 +116,11 @@ uint32_t bits_to_digit(uint32_t value) {
 		case 81: return 7;
 		case 127: return 8;
 		case 95: return 9;
+		// тут нужно возвращать некий флаг ошибки
+		// в ином случае пустые знакоместа 
+		// и всякие вещи типа "-" и "HI" будут
+		// принудительно приведены к нулю
+		default: return 0;
 	}
 }
 
@@ -135,29 +159,29 @@ ISR(SPI_STC_vect) {
 	switch (SPI_STATE++) {
 
 		case 0: if (value != SPI_PACKET_START) SPI_STATE = SPI_STATE_START; break;
-		case 4: SPI_TEMPERATURE = T_D2(value, 0) | TL_D2(value, 1) | BL_D2(value, 2) | NEGATIVE_D(value, 3) | TR_D2(value, 4) | C_D2(value, 5) | BR_D2(value, 6) | B_D2(value, 7); break;
-		case 5: SPI_TEMPERATURE |= T_D1(value, 0) | TL_D1(value, 1) | BL_D1(value, 2) | TR_D1(value, 4) | C_D1(value, 5) | BR_D1(value, 6) | B_D1(value, 7); break;
+		case 4: SPI_TEMPERATURE = D1_T(value, 0) | D1_TL(value, 1) | D1_BL(value, 2) | D_NEGATIVE(value, 3) | D1_TR(value, 4) | D1_C(value, 5) | D1_BR(value, 6) | D1_B(value, 7); break;
+		case 5: SPI_TEMPERATURE |= D0_T(value, 0) | D0_TL(value, 1) | D0_BL(value, 2) | D0_TR(value, 4) | D0_C(value, 5) | D0_BR(value, 6) | D0_B(value, 7); break;
 		case 10: if (value >> 6 != SPI_PACKET1_END) SPI_STATE = SPI_STATE_START; break;
 
 		case 11: if (value != SPI_PACKET_START) SPI_STATE = SPI_STATE_START; break;
-		case 18: SPI_METERAGE = T_D3(value, 4) | TL_D3(value, 5) | BL_D3(value, 6) | D4(value, 7); break;
-		case 19: SPI_METERAGE |= TR_D3(value, 0) | C_D3(value, 1) | BR_D3(value, 2) | B_D3(value, 3) | T_D2(value, 4) | TL_D2(value, 5) | BL_D2(value, 6); break;
-		case 20: SPI_METERAGE |= TR_D2(value, 0) | C_D2(value, 1) | BR_D2(value, 2) | B_D2(value, 3); break;
+		case 18: SPI_METERAGE = D2_T(value, 4) | D2_TL(value, 5) | D2_BL(value, 6) | D3(value, 7); break;
+		case 19: SPI_METERAGE |= D2_TR(value, 0) | D2_C(value, 1) | D2_BR(value, 2) | D2_B(value, 3) | D1_T(value, 4) | D1_TL(value, 5) | D1_BL(value, 6); break;
+		case 20: SPI_METERAGE |= D1_TR(value, 0) | D1_C(value, 1) | D1_BR(value, 2) | D1_B(value, 3); break;
 		case 21: if (value >> 6 != SPI_PACKET2_END) SPI_STATE = SPI_STATE_START; break;
 
 		case 22: if (value != SPI_PACKET_START) SPI_STATE = SPI_STATE_START; break;
-		case 23: SPI_METERAGE |= T_D1(value, 0) | TL_D1(value, 1) | BL_D1(value, 2) | TR_D1(value, 4) | C_D1(value, 5) | BR_D1(value, 6) | B_D1(value, 7); break;
+		case 23: SPI_METERAGE |= D0_T(value, 0) | D0_TL(value, 1) | D0_BL(value, 2) | D0_TR(value, 4) | D0_C(value, 5) | D0_BR(value, 6) | D0_B(value, 7); break;
 		case 24: SPI_METERAGE_UNIT = value; break;
-		case 28: SPI_METERAGE |= FLOAT_D(value, 7); break;
+		case 28: SPI_METERAGE |= D_FLOAT(value, 7); break;
 		case 32: if (value >> 6 != SPI_PACKET3_END) SPI_STATE = SPI_STATE_START; break;
 
 		case 33: if (value != SPI_PACKET_START) SPI_STATE = SPI_STATE_START; break;
-		case 36: SPI_TIME = D4(value, 1) | B_D3(value, 2) | BL_D3(value, 3) | C_D3(value, 6) | TL_D3(value, 7); break;
-		case 37: SPI_TIME |= BR_D3(value, 2) | TR_D3(value, 3) | T_D3(value, 7); break;
-		case 38: SPI_TIME |= B_D2(value, 2) | BL_D2(value, 3) | C_D2(value, 6) | TL_D2(value, 7); break;
-		case 39: SPI_TIME |= BR_D2(value, 2) | TR_D2(value, 3) | T_D2(value, 7); break;
-		case 40: SPI_TIME |= B_D1(value, 2) | BL_D1(value, 3) | C_D1(value, 6) | TL_D1(value, 7); break;
-		case 41: SPI_TIME |= BR_D1(value, 2) | TR_D1(value, 3) | T_D1(value, 7); break;
+		case 36: SPI_TIME = D3(value, 1) | D2_B(value, 2) | D2_BL(value, 3) | D2_C(value, 6) | D2_TL(value, 7); break;
+		case 37: SPI_TIME |= D2_BR(value, 2) | D2_TR(value, 3) | D2_T(value, 7); break;
+		case 38: SPI_TIME |= D1_B(value, 2) | D1_BL(value, 3) | D1_C(value, 6) | D1_TL(value, 7); break;
+		case 39: SPI_TIME |= D1_BR(value, 2) | D1_TR(value, 3) | D1_T(value, 7); break;
+		case 40: SPI_TIME |= D0_B(value, 2) | D0_BL(value, 3) | D0_C(value, 6) | D0_TL(value, 7); break;
+		case 41: SPI_TIME |= D0_BR(value, 2) | D0_TR(value, 3) | D0_T(value, 7); break;
 		case 43: if (value >> 6 != SPI_PACKET4_END) SPI_STATE = SPI_STATE_START; else SPI.detachInterrupt(); break;
 
 	}
