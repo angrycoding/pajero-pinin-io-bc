813bb0b9c4d01c18fad66f3da35fb0bcede58ae5
Ruslan <matveev.ruslan@gmail.com>
2017-04-20 16:42:47 +0300

    async K-line init

--- firmware/KL.h
+++ firmware/KL.h
@@ -3,68 +3,160 @@
 
 #include <SoftwareSerial.h>
 
+/*
+#define KL_STATE_B0_START 0
+#define KL_STATE_STARTBIT 1
+#define KL_STATE_BIT0 2
+#define KL_STATE_BIT1_7 3
+#define KL_STATE_STOPBIT 4
+#define KL_STATE_SERIAL_BEGIN 5
+#define KL_STATE_SYNC 6
+#define KL_STATE_REQUEST_PID 7
+*/
+
 namespace KL_private {
 
-	uint8_t pinRX;
-	uint8_t pinTX;
+	uint8_t PIN_RX;
+	uint8_t PIN_TX;
+	// uint8_t state;
+	// uint32_t actionTime;
 	SoftwareSerial *klSerial;
 
 }
 
 namespace KL {
 
-	void init(uint8_t pinRX, uint8_t pinTX) {
+	void init(uint8_t PIN_RX, uint8_t PIN_TX) {
 		using namespace KL_private;
-		pinMode(KL_private::pinRX = pinRX, INPUT);
-		pinMode(KL_private::pinTX = pinTX, OUTPUT);
-		klSerial = new SoftwareSerial(pinRX, pinTX);
+		// actionTime = 0;
+		// state = KL_STATE_B0_START;
+		pinMode(KL_private::PIN_RX = PIN_RX, INPUT);
+		pinMode(KL_private::PIN_TX = PIN_TX, OUTPUT);
+		klSerial = new SoftwareSerial(PIN_RX, PIN_TX);
 	}
+
+	/*
+	void update() {
+		using namespace KL_private;
+		switch (state) {
+
+			case KL_STATE_ERROR:
+				klSerial->end();
+				state = KL_STATE_B0_START;
+
+			case KL_STATE_B0_START:
+				digitalWrite(PIN_TX, HIGH);
+				actionTime = millis();
+				state = KL_STATE_B0_END;
+
+			case KL_STATE_B0_END:
+				if (millis() - actionTime < 300) break;
+				state = KL_STATE_B1_START;
+
+			case KL_STATE_B1_START:
+				digitalWrite(PIN_TX, LOW);
+				actionTime = millis();
+				state = KL_STATE_B1_END;
+
+			case KL_STATE_B1_END:
+				if (millis() - actionTime < 200) break;
+				state = KL_STATE_B2_START;
+
+			case KL_STATE_B2_START:
+				digitalWrite(PIN_TX, HIGH);
+				actionTime = millis();
+				state = KL_STATE_B2_END;
+
+			case KL_STATE_B2_END:
+				if (millis() - actionTime < 200) break;
+				state = KL_STATE_B3_START;
+
+			case KL_STATE_B3_START:
+				digitalWrite(PIN_TX, LOW);
+				actionTime = millis();
+				state = KL_STATE_B3_END;
+
+			case KL_STATE_B3_END:
+				if (millis() - actionTime < 1400) break;
+				state = KL_STATE_B4_START;
+
+			case KL_STATE_B4_START:
+				digitalWrite(PIN_TX, HIGH);
+				actionTime = millis();
+				state = KL_STATE_B4_END;
+
+			case KL_STATE_B4_END:
+				if (millis() - actionTime < 200) break;
+				state = KL_STATE_B5_START;
+
+			case KL_STATE_B5_START:
+				klSerial->begin(15625);
+				actionTime = millis();
+				state = KL_STATE_B5_END;
+
+			case KL_STATE_B5_END:
+				
+				if (millis() - actionTime > 1000 || (klSerial->available() == 3 && (
+					klSerial->read() != 0x55 ||
+					klSerial->read() != 0xEF ||
+					klSerial->read() != 0x85
+				))) {
+					state = KL_STATE_ERROR;
+					break;
+				}
+
+				if (klSerial->available() != 3) break;
+				state = KL_STATE_B6_START;
+
+		}
+	}
+	*/
 
 	void connect() {
 		using namespace KL_private;
-		
+
 		// wakeup
-		digitalWrite(pinTX, HIGH);
+		digitalWrite(PIN_TX, HIGH);
 		delay(300);
 
 		// startbit
-		digitalWrite(pinTX, LOW);
+		digitalWrite(PIN_TX, LOW);
 		delay(200);
 
 		// bit0
-		digitalWrite(pinTX, HIGH);
+		digitalWrite(PIN_TX, HIGH);
 		delay(200);
 
 		// bit1
-		digitalWrite(pinTX, LOW);
+		digitalWrite(PIN_TX, LOW);
 		delay(200);
 
 		// bit2
-		digitalWrite(pinTX, LOW);
+		digitalWrite(PIN_TX, LOW);
 		delay(200);
 
 		// bit3
-		digitalWrite(pinTX, LOW);
+		digitalWrite(PIN_TX, LOW);
 		delay(200);
 
 		// bit4
-		digitalWrite(pinTX, LOW);
+		digitalWrite(PIN_TX, LOW);
 		delay(200);
 
 		// bit5
-		digitalWrite(pinTX, LOW);
+		digitalWrite(PIN_TX, LOW);
 		delay(200);
 
 		// bit6
-		digitalWrite(pinTX, LOW);
+		digitalWrite(PIN_TX, LOW);
 		delay(200);
 
 		// bit7
-		digitalWrite(pinTX, LOW);
+		digitalWrite(PIN_TX, LOW);
 		delay(200);
 
 		// stopbit
-		digitalWrite(pinTX, HIGH);
+		digitalWrite(PIN_TX, HIGH);
 		delay(200);
 
 
